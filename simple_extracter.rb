f86dd0031108e7fa115739fa6b4c1a6e393fd013
Ryan Taylor <ryanmt@byu.net>
2012-10-24 18:11:19 -0600

    fixed the output option to automatically generate a filename

--- simple_extracter.rb
+++ simple_extracter.rb
@@ -21,7 +21,10 @@ parser = OptionParser.new do |opts|
     puts opts
     exit
   end
-  opts.on('-o STRING', "--output STRING", "Give me a file name for a yml file output of the information, with an attempt to correlate the information by charge states and hopefully by chromatography (coming soon)") do |outfile|
+  opts.on('-o STRING', "--output STRING", "Give me a file name (or 'auto' for automatic filename generation) for a yml file output of the information, with an attempt to correlate the information by charge states and hopefully by chromatography (coming soon)") do |outfile|
+    if outfile == 'auto'
+      outfile = File.absolute_path(ARGV.first).sub('.mzML', ".yml")
+    end
     options[:output_results] = outfile
   end
 
